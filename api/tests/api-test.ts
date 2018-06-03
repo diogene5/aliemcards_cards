3ead78678de5229aac6761d2e04c48a2bba19bbc
Jeremy Voros <jeremyvoros@gmail.com>
2018-06-03 16:15:24 -0600

    image dir check

--- api/tests/api-test.ts
+++ api/tests/api-test.ts
@@ -1,6 +1,9 @@
-import * as path from 'path';
-import CardFactory from '../build/CardFactory';
 import { readdirSync, readFileSync } from 'fs';
+import * as path from 'path';
+
+import config from '../build/config';
+import CardFactory from '../build/CardFactory';
+
 
 // run build on test cards
 const cf = new CardFactory(path.join(__dirname, './cards'), path.join(__dirname, './dist'));
@@ -24,4 +27,21 @@ endpoints.forEach(ep => {
   if (snap.compare(test) !== 0) throw Error(`Error in ${ep} build`);
 });
 
-// compare image directories
+// compare built image directories to snapshop image directory
+interface imageTree {
+  [dir: string]: string[]
+}
+
+function buildTree(dir: string): imageTree {
+  const tree = {};
+  const slugs = readdirSync(path.join(__dirname, dir));
+  slugs.forEach(slug => {
+    const images = readdirSync(path.join(__dirname, `${dir}/${slug}`)).filter(x => x.match(config.REGEX.image_file) !== null);
+    if (images.length > 0) tree[slug] = images;
+  });
+  return tree;
+}
+
+const a = JSON.stringify(buildTree('./snapshots/images'));
+const b = JSON.stringify(buildTree('./dist/images'));
+if ( a !== b) throw Error(`Error in images build`);

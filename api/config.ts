38dd930714af24e026ed1e8803f7c1c6d72a5633
Jeremy Voros <jeremyvoros@gmail.com>
2018-08-17 13:03:21 -0600

    finally basic functionality

--- api/config.ts
+++ api/config.ts
@@ -1,13 +1,12 @@
 import * as path from 'path';
 
 export default {
-  API_VERSION: 'v1',
   DIST_DIR: path.resolve(__dirname, '../dist'),
   CARDS_DIR: path.resolve(__dirname, '../cards'),
   IGNORED_FILES: ['.DS_Store', 'announcements.yml', 'media'],
-  IMG_URL_PREFIX: 'https://aliemcardsapi.netlify.com/v1/media',
+  IMG_URL_PREFIX: 'https://aliemcardsapi.netlify.com',
   REGEX: {
-    image_url: new RegExp(/[\w-]+\.(?:png|jpg|jpeg|gif)/, 'gi'),
+    image_url: new RegExp(/media\/[\w-]+\.(?:png|jpg|jpeg|gif)/, 'gi'),
     image_file: new RegExp(/\.(gif|jpg|jpeg|tiff|png)$/, 'i'),
     markdownH1: new RegExp(/^#(?!#).+/, 'm'),
     frontmatter: new RegExp(/(^-{3}(?:\r\n|\r|\n)([\w\W]*?)-{3}(?:\r\n|\r|\n))?([\w\W]*)*/),

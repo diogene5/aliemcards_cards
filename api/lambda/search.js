933af19405984a1b9c35abb9e22a8c7f422455fe
Jeremy Voros <jeremyvoros@gmail.com>
2018-08-19 12:02:29 -0600

    most basic

--- api/lambda/search.js
+++ api/lambda/search.js
@@ -1,27 +1,27 @@
-'use strict';
+// 'use strict';
 
-const cards = require('../dist/cards.json').cards;
-const Fuse = require('fuse.js');
+// const cards = require('../dist/cards.json').cards;
+// const Fuse = require('fuse.js');
 
-const fuse = new Fuse(cards, {
-  caseSensitive: false,
-  shouldSort: true,
-  tokenize: true,
-  threshold: 0.2,
-  location: 0,
-  distance: 0,
-  maxPatternLength: 20,
-  minMatchCharLength: 3,
-  keys: [{ name: 'title', weight: 0.8 }, { name: 'body', weight: 0.2 }],
-});
+// const fuse = new Fuse(cards, {
+//   caseSensitive: false,
+//   shouldSort: true,
+//   tokenize: true,
+//   threshold: 0.2,
+//   location: 0,
+//   distance: 0,
+//   maxPatternLength: 20,
+//   minMatchCharLength: 3,
+//   keys: [{ name: 'title', weight: 0.8 }, { name: 'body', weight: 0.2 }],
+// });
 
-function respond(obj) {
-  return {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin' : '*' },
-    body: JSON.stringify(obj)
-  }
-}
+// function respond(obj) {
+//   return {
+//     statusCode: 200,
+//     headers: { 'Access-Control-Allow-Origin' : '*' },
+//     body: JSON.stringify(obj)
+//   }
+// }
 
 // exports.handler = (event, context, callback) => {
 //   callback(null, respond({ message: 'here you are', event }));

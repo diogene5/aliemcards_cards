490aa90ee99152d85c48a5a51e54dc528b8828b4
Jeremy Voros <jeremy@Jeremys-MacBook-Pro.local>
2019-10-29 12:56:32 -0600

    new image regex

--- api/config.ts
+++ api/config.ts
@@ -6,7 +6,7 @@ export default {
   IGNORED_FILES: ['.DS_Store', 'announcements.yml', 'media'],
   IMG_URL_PREFIX: 'https://aliemcards.netlify.com',
   REGEX: {
-    image_url: new RegExp(/media\/[\w-]+\.(?:png|jpg|jpeg|gif)/, 'gi'),
+    image_url: new RegExp(/\/?media\/[\w-]+\.(?:png|jpg|jpeg|gif)/, 'gi'),
     image_file: new RegExp(/\.(gif|jpg|jpeg|tiff|png)$/, 'i'),
     markdownH1: new RegExp(/^#(?!#).+/, 'm'),
     frontmatter: new RegExp(/(^-{3}(?:\r\n|\r|\n)([\w\W]*?)-{3}(?:\r\n|\r|\n))?([\w\W]*)*/),
